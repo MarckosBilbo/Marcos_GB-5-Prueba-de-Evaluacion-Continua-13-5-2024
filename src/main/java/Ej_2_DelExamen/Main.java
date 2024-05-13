42dd05cd55d079724df24061f13d16807bf9f423
marcos <marcgabe@myuax.com>
2024-05-13 15:01:09 +0200

    Borrado de paquetes excedentess (ej 2)

--- src/main/java/Ej_2_DelExamen/Main.java
+++
@@ -1,4 +0,0 @@
-package Ej_2_DelExamen;
-
-public class Main {
-}
---
+++ src/test/java/Ej_1_DelExamen/Entrega2/RestaurantComponent.java
@@ -0,0 +1,12 @@
+package Ej_1_DelExamen.Entrega2;
+
+
+/**
+ * Interface for different types of restaurant components.
+ */
+public interface RestaurantComponent {
+    /**
+     * Method to serve a customer.
+     */
+    void serveCustomer();
+}
